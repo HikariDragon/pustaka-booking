ab558d4a97c335f14d7513d58e4a293baba21457
HikariDragon <aufarafiqih2@gmail.com>
2022-10-07 16:10:25 +0700

    menambah about

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,18 +2,18 @@
  <h1><?php echo $judul ?></h1>
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Imam</li>
- <li>Nama Belakang : Nawawi</li>
+ <li>Nama Depan : Hikari</li>
+ <li>Nama Belakang : Aufa Rafiqi</li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+ <li> Rejosari</li>
  </ul>
  
  <h4>Tempat Lahir</h4>
  <ul type="none">
- <li>Cirebon</li>
+ <li>Surakarta</li>
  </ul>
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
